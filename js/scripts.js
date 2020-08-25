17f9b5e1a4ba8ec1f9273f05b974f00e7daa3959
Louis Atkinson <atkinsonlouis@gmail.com>
2020-08-25 17:15:18 +0300

    completed the extra tasks

--- js/scripts.js
+++ js/scripts.js
@@ -26,3 +26,119 @@ document.getElementById("title-wrapper").style =
 
 document.getElementById("progress").style =
   "width: fit-content; margin: auto; background: black; color: white; ";
+
+/****** extra tasks *******/
+
+/****** Part One *******/
+/* creation of unordered list and addding it into document body */
+
+var ul = document.createElement("ul");
+document.body.append(ul);
+
+/****** First LI *******/
+/* Creates first LI element and adds it into the unordered list */
+
+var firstLi = document.createElement("li");
+ul.append(firstLi);
+
+/* creation and specification of the attributes of the first A element which is nested under the first li element */
+
+var firstA = document.createElement("a");
+firstA.setAttribute("id", "home");
+firstA.setAttribute("class", "btn");
+firstA.setAttribute("href", "default.asp");
+firstA.innerHTML = "Home";
+
+/* appends the first A to the first LI element */
+
+firstLi.append(firstA);
+
+/****** Second LI *******/
+/* Creates second LI element and adds it into the unordered list */
+
+var secondLi = document.createElement("li");
+ul.append(secondLi);
+
+/* creation and specification of the attributes of the second A element which is nested under the second li element */
+
+var secondA = document.createElement("a");
+secondA.setAttribute("id", "news");
+secondA.setAttribute("class", "btn");
+secondA.setAttribute("href", "news.asp");
+secondA.innerHTML = "News";
+
+/* appends the second A to the second LI element */
+
+secondLi.append(secondA);
+
+/****** Third LI *******/
+/* Creates second LI element and adds it into the unordered list */
+
+var thirdLi = document.createElement("li");
+ul.append(thirdLi);
+
+/* creation and specification of the attributes of the third  A element which is nested under the third li element */
+
+var thirdA = document.createElement("a");
+thirdA.setAttribute("id", "contact");
+thirdA.setAttribute("class", "btn");
+thirdA.setAttribute("href", "contact.asp");
+thirdA.innerHTML = "Contact";
+
+/* appends the third A to the third LI element */
+
+thirdLi.append(thirdA);
+
+/****** Fourth LI *******/
+/* Creates Fourth LI element and adds it into the unordered list */
+var fourthLi = document.createElement("li");
+ul.append(fourthLi);
+
+/* creation and specification of the attributes of the fourth A element which is nested under the fourth li element */
+
+var fourthA = document.createElement("a");
+fourthA.setAttribute("id", "about");
+fourthA.setAttribute("class", "btn");
+fourthA.setAttribute("href", "#");
+fourthA.innerHTML = "About";
+
+/* appends the first A to the first LI element */
+
+fourthLi.append(fourthA);
+
+/****** Part Two *******/
+
+var content = document.createElement("div");
+content.setAttribute("id", "content");
+
+/* document.addEventListener("click", function () {
+  content.innerHTML = "Welcome!";
+});
+*/
+
+/* sets the document title to the var definied below */
+
+var titler = document.getElementsByTagName("title")[0];
+
+/* sets the document title to the var definied below */
+
+var loadAbout = function () {
+  document.body.appendChild(content);
+};
+
+/* Sets the inner html of content to the specified value, however this is only executed when the event listener is called. */
+
+content.innerHTML = "Welcome to my website";
+
+/* sets the display of the document title to none*/
+
+var hide = function () {
+  titler.style.display = "none";
+};
+
+/* when the about button is clicked is executes the loadAbout and hide functions.*/
+
+document.getElementById("about").addEventListener("click", () => {
+  loadAbout();
+  hide();
+});
